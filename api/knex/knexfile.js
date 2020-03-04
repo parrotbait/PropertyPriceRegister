b8f17351d06388830b434e0b46aab8336a7e9f90
Eddie Long <eddie@pushfor.com>
2020-03-04 22:16:33 +0000

    Docker working now :)

--- api/knex/knexfile.js
+++ api/knex/knexfile.js
@@ -4,7 +4,8 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: 'localhost',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 3306,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
